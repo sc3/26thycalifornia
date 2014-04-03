8df2f86b264efb277f4654bb103bff139b1a1987
David Eads <davideads@gmail.com>
2014-04-03 02:27:47 -0500

    incorporar parse funcion

--- js/Data.js
+++ js/Data.js
@@ -1,3 +1,11 @@
 var DailyPopulationCollection = Backbone.Collection.extend({
-  url: 'data/daily_population.json'
+  url: 'data/daily_population.json',
+  parse: function(data) {
+    // Cast all keys to numbers
+    return _.map(data, function(day) {
+      for (key in day)
+          day[key] = Number(day[key]);
+      return day;
+    });
+  }
 });
