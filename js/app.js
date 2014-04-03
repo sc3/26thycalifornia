365bd4e8917d1f0de880e8abe90c97057721d2e6
David Eads <davideads@gmail.com>
2014-04-03 02:32:10 -0500

    incorporar una nueva StatsTableView en app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   var population = new DailyPopulationCollection();
-  population.on("sync", function() {
-    console.log("Average:", this.average('population'));
-    console.log("Min:", this.get_min('population'));
-    console.log("Max:", this.get_max('population'));
+  var population_table = new StatsTableView({
+    el: $("#poblacion-diaria .stats"),
+    collection: population,
+    template: $('#population-table-template').html(),
   });
   population.fetch();
 });
