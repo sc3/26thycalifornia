c4d2ba06f0a8fea229b56139ba2a3ab543d5f4fa
David Eads <davideads@gmail.com>
2014-04-03 18:13:58 -0500

    add basic chart code

--- js/Charts.js
+++ js/Charts.js
@@ -13,7 +13,54 @@ var StatsTableView = Backbone.View.extend({
 
 var DailyPopulationChartView = ChartView.extend({
   draw: function() {
-    this.$el.html('¡Hola mundo!');
+    var view = this;
+
+    // Get data
+    var maxDay = this.collection.get_max("population");
+    var data = this.collection.toJSON();
+
+    // A function to set y values for bars
+    var yScale = d3.scale.linear()
+      .range([this.dimensions.height, 0])
+      .domain([0, maxDay.get("population")])
+      ;
+
+    // A function to set x positions for bars
+    var startDate = new Date(data[0].date);
+    var endDate = new Date(data[this.data.length - 1].date);
+    var xScale = d3.time.scale()
+      .range([0, this.dimensions.width])
+      .domain([startDate, endDate])
+      ;
+
+    // Create canvas
+    var svg = d3.select(this.el).append("svg")
+        .attr("width", this.dimensions.wrapperWidth)
+        .attr("height", this.dimensions.wrapperHeight)
+      .append("g")
+        .attr("transform", "translate(" + this.options.margin.left + "," + this.options.margin.top + ")")
+        ;
+
+    svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d, i) {
+        var date = new Date(d.date);
+        return xScale(date);
+      })
+      .attr("y", function(d) {
+        return yScale(d.population);
+      })
+      .attr("width", function(d) {
+        return view.dimensions.width / data.length;
+      })
+      .attr("height", function(d) {
+        return view.dimensions.height - yScale(d.population);
+      })
+      ;
+
     return this;
   },
 });
